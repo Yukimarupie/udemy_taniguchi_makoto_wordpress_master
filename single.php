91c785d005979c17cd2d66a7f4991c25d3358beb
Yukimarupie <y.harada59@gmail.com>
2021-01-10 23:06:21 +0900

    26 アイキャッチ画像のサイズ指定や、デフォルト画像の表示

--- single.php
+++ single.php
@@ -13,13 +13,20 @@
 
   <?php if (have_posts()) : ?>
     <?php while (have_posts()) : the_post(); ?>
-
+      <!-- Page Header -->
       <?php
-      $id = get_post_thumbnail_id();
-      $img = wp_get_attachment_image_src($id); 
+      if (has_post_thumbnail()): 
+        $id = get_post_thumbnail_id();
+        $img = wp_get_attachment_image_src($id, 'large');//fullが一番画質がいいが、largeの方がファイルサイズが小さくバランスが取れる。もしさらに大きく使いたい場合は管理画面の設定＞メディアから変更可能
+      else:
+        $img = array(get_template_directory_uri() . '/img/post-bg.jpg');//アイキャッチが設定されていない場合はデフォルトでこれを表示するよ
+      endif;
       ?>
 
-      <!-- Page Header -->
+        
+
+
+
       <header class="masthead" style="background-image: url('<?php echo $img[0]; ?>')">
         <div class="overlay"></div>
         <div class="container">

06e66862992da8bf4f1630519519417f5503d30c
Yukimarupie <y.harada59@gmail.com>
2021-01-10 21:10:19 +0900

    絶対パスの修正

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
  <!-- Bootstrap core JavaScript -->
- <script src="http://mysite.local/wp-content/themes/clean_blog/vendor/jquery/jquery.min.js"></script>
-  <script src="http://mysite.local/wp-content/themes/clean_blog/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+ <script src="<?php echo get_template_directory_uri(); ?>/vendor/jquery/jquery.min.js"></script>
+ <script src="<?php echo get_template_directory_uri(); ?>/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-  <!-- Custom scripts for this template -->
-  <script src="http://mysite.local/wp-content/themes/clean_blog/js/clean-blog.min.js"></script>
+ <!-- Custom scripts for this template -->
+ <script src="js/clean-blog.min.js"></script>
 
-  <?php wp_footer(); ?>
+ <?php wp_footer(); ?>
